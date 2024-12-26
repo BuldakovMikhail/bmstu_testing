d166a02dab8ded8f85d0c26dd7bdea175a09f056
Buldakov Mikhail <mishabul2003@gmail.com>
2024-12-27 01:10:13 +0300

    init

--- src/muzyaka/internal/models/album.go
+++
@@ -1,8 +0,0 @@
-package models
-
-type Album struct {
-	Id        uint64
-	Name      string
-	CoverFile []byte
-	Type      string
-}
--- src/muzyaka/internal/models/musician.go
+++
@@ -1,8 +0,0 @@
-package models
-
-type Musician struct {
-	Id          uint64
-	Name        string
-	PhotoFiles  [][]byte
-	Description string
-}
